7501d5efe7df5d63a92a0631afb42baa6fc3222b
ahin0422 <ahinahn0422@gmail.com>
2026-06-25 14:55:33 +0900

    add

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-let welocation = document.getElementById('location');
+let button = document.getElementById('button');
+
+button.onclick = function() {
+    alert('정상적으로 제출되었습니다.')
+}
+
+
+let welocation = document.getElementById('location'); // 변수
 let mapImages = document.getElementsByClassName('map-img');
 let cardHeader = document.getElementsByClassName('card-header');
 
-welocation.onclick = function() {
+welocation.onclick = function() { // 함수
     welocation.innerHTML = "현재 지역 : <달성군 구지면>";
 
     cardHeader[0].innerHTML = "📍 홈푸드마트 앞"
@@ -11,8 +18,7 @@ welocation.onclick = function() {
     cardHeader[3].innerHTML = "📍 벚꽃공원"
 
     mapImages[0].src = "../images/안전구역-4.jpeg";
-    mapImages[1].src = "../images/안전구역3.jpg";
-    mapImages[2].src = "../images/안전구역2.jpg";
-    mapImages[3].src = "../images/안전구역1.jpg";
-
+    mapImages[1].src = "../images/안전구역-3.jpg";
+    mapImages[2].src = "../images/안전구역-2.jpg";
+    mapImages[3].src = "../images/안전구역-1.jpg";
 }
